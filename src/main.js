175612990fc00f428e17f294195638ac28e7d43a
Andrej Podlubnyj <Andrej_Podlubnyj@epam.com>
2023-03-28 05:34:21 +0200

    feat: add live edit with prettier

--- src/main.js
+++ src/main.js
@@ -25,15 +25,15 @@ window.onload = () => {
 
     // Labels and delays in seconds
     .addLabel('startFrame1', 0)
-    .addLabel('startFrame2', 4)
+            .addLabel('startFrame2', 4)
     .addLabel('startFrame3', 8)
     .addLabel('startFrame4', 13)
 
     // Frame 1
-    .to('.banner', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame1')
-    .to('#frame1 .accompany', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame1+=0.5')
-    .set('#frame1 .line', { className: 'line expanded' }, 'startFrame1+=1')
-    .to('#frame1 .copy', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame1+=1.5')
+    .to('.banner', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame1')
+    .to('#frame1 .accompany', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame1+=0.5')
+    .set('#frame1 .line', {className: 'line expanded'}, 'startFrame1+=1')
+    .to('#frame1 .copy', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame1+=1.5')
     .to(
       '#frame1 .accompany, #frame1 .copy, #frame1 .line',
       {
@@ -45,27 +45,27 @@ window.onload = () => {
     )
 
     // Frame 2
-    .to('#frame2', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame2')
-    .to('#frame2 .copy', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame2+=0.5')
-    .to('#frame2 .accompany', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame2+=1')
+    .to('#frame2', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame2')
+    .to('#frame2 .copy', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame2+=0.5')
+    .to('#frame2 .accompany', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame2+=1')
 
     // Frame 3
-    .to('#frame2 .rect', { bottom: 18, ease: 'power4.out', duration: 0.5 }, 'startFrame3')
-    .set('#frame2 .rect', { className: 'rect expanded' }, 'startFrame3')
-    .to('#frame3', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame3+=.5')
-    .to('#frame3 .copy', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame3+=.7')
-    .to('#frame3 .accompany', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame3+=2')
+    .to('#frame2 .rect', {bottom: 18, ease: 'power4.out', duration: 0.5}, 'startFrame3')
+    .set('#frame2 .rect', {className: 'rect expanded'}, 'startFrame3')
+    .to('#frame3', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame3+=.5')
+    .to('#frame3 .copy', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame3+=.7')
+    .to('#frame3 .accompany', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame3+=2')
 
     // Frame 4
-    .to('#frame4', { opacity: 1, ease: 'none', duration: 0.5 }, 'startFrame4')
-    .to('#frame4 .content', { bottom: 0, ease: 'power4.out', duration: 1 }, 'startFrame4+=.5')
+    .to('#frame4', {opacity: 1, ease: 'none', duration: 0.5}, 'startFrame4')
+    .to('#frame4 .content', {bottom: 0, ease: 'power4.out', duration: 1}, 'startFrame4+=.5')
 }
 
 // init scroll after animation
 function initScroll() {
   setTimeout(() => {
-    customScroll.scroll({ y: '0%' })
-    customScroll.scroll([0, '100%'], 103000, { y: 'linear' })
+    customScroll.scroll({y: '0%'})
+    customScroll.scroll([0, '100%'], 103000, {y: 'linear'})
   }, initScrollDelay * 1000)
 }
 
